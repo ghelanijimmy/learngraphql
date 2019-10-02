feb12d8f3b8573d7f244399493684274eab49c7c
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2019-10-01 23:16:26 -0400

    correct docker set up and initialize spinner code

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  console.log(process.env);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <svg className={"spinner"} height={100} width={100}>
+        <circle r={10} fill={"black"} cx={15} cy={30} />
+        <circle r={10} fill={"black"} cx={50} cy={10} />
+        <circle r={10} fill={"black"} cx={85} cy={30} />
+        <circle r={10} fill={"black"} cx={85} cy={70} />
+        <circle r={10} fill={"black"} cx={50} cy={90} />
+        <circle r={10} fill={"black"} cx={15} cy={70} />
+      </svg>
     </div>
   );
 }
